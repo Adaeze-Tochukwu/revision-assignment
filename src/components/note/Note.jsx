4c493fabdd690d115a0e779dbc4b81ed07dd1ae7
Adaeze-Tochukwu <adarose.tochukwu@gmail.com>
2023-05-23 22:49:59 +0100

    added note component

--- src/components/note/Note.jsx
+++ src/components/note/Note.jsx
@@ -2,5 +2,14 @@ import React from "react";
 import "./Note.scss";
 
 export default function Note() {
-  return <div className="note">Note</div>;
+  return (
+    <div className="note">
+      <h3>Lesson note</h3>
+      <textarea type="text" placeholder="Type here"></textarea>
+      <div className="note__btn">
+        <a href="" className="note__btn--reset">Reset</a>
+        <a href="" className="note__btn--save">Save</a>
+      </div>
+    </div>
+  );
 }
